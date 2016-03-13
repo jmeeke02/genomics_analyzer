108d5c6340ce7f471b9ae8633dd322cc394bf4a4
Jordan Meeker <jordanwmeeker@gmail.com>
2016-03-13 14:22:37 -0400

    adds conversion steps to json converter

--- server/RawtoJSON.js
+++ server/RawtoJSON.js
@@ -7,8 +7,11 @@ var rootPath = path.join(__dirname, '../');
  //turns the raw text data into a JSON file that can be interpreted by SNPs
 module.exports = function(){
 	//this is a synchronous operation, no need to promisify https://github.com/petkaantonov/bluebird/issues/225
-  var text = fs.readFileSync(path.join(rootPath, "data/user-upload.txt"));
+  var text = fs.readFileSync(path.join(rootPath, "data/raw/user-upload.txt"));
+  text = JSON.stringify(text);
+  console.log(typeof text);
   dna.parse(text, function(err, snps){
-  	fs.writeFileSync(path.join(rootPath, "data/user-upload.json"), JSON.stringify(snps));
+  	if(err) console.log(err);
+  	fs.writeFileSync(path.join(rootPath, "data/json/user-upload.json"), JSON.stringify(snps));
   })
 };
