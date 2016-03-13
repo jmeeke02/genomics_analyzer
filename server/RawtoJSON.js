5671c1457583b3f741620a095789187dcb2dec28
Jordan Meeker <jordanwmeeker@gmail.com>
2016-03-13 14:11:49 -0400

    refactor raw to json converter

--- server/RawtoJSON.js
+++ server/RawtoJSON.js
@@ -1,12 +1,14 @@
 var dna = require('dna2json');
 var JSONStream = require('JSONStream');
 var fs = require('fs');
+var path = require('path');
+var rootPath = path.join(__dirname, '../');
 
  //turns the raw text data into a JSON file that can be interpreted by SNPs
 module.exports = function(){
 	//this is a synchronous operation, no need to promisify https://github.com/petkaantonov/bluebird/issues/225
-  fs.createReadStream("../data/user-upload.txt")
-  .pipe(dna.createParser())
-  .pipe(JSONStream.stringify())
-  .pipe(fs.createWriteStream("../data/user-upload.json"));
+  var text = fs.readFileSync(path.join(rootPath, "data/user-upload.txt"));
+  dna.parse(text, function(err, snps){
+  	fs.writeFileSync(path.join(rootPath, "data/user-upload.json"), JSON.stringify(snps));
+  })
 };
